488bed2071c390d24127e10ec766d764499ee00b
PJTURP <821042@mystma.org>
2021-09-21 09:25:40 -0500

    attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#def topFiveMovies
+def topFiveMovies(a, b, c, d, e):
+    return(a +" " + b + " " + c + " " + d+ " " + e + " " )
+
+#run function
+print(topFiveMovies("Black Sheep", "Water Boy", "Happy Gilmore", "Ford vs Ferrari", "Top Gear"))
